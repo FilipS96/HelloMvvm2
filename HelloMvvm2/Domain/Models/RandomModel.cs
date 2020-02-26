d99638805cc79354805cc7a490c550f3f8867dc1
Filip Stojanovski <filip.stojanovski@afconsult.com>
2020-02-26 15:20:52 +0100

    update 2

--- HelloMvvm2/Domain/Models/RandomModel.cs
+++ HelloMvvm2/Domain/Models/RandomModel.cs
@@ -7,6 +7,13 @@ namespace HelloMvvm2.Domain.Models
     public class RandomModel : ViewModelBase
     {
         private Random _rndNumber;
+        private int _rndOne;
+        private int _rndTwo;
+        private int _rndThree;
+
+        private ObservableCollection<int> _listOne;
+        private ObservableCollection<int> _listTwo;
+        private ObservableCollection<int> _listThree;
 
         public RandomModel()
         {
@@ -19,5 +26,42 @@ namespace HelloMvvm2.Domain.Models
             get => _rndNumber;
             set => SetProperty(ref _rndNumber, value);
         }
+
+        public int RndOne
+        {
+            get => _rndOne;
+            set => SetProperty(ref _rndOne, value);
+        }
+
+        public int RndTwo
+        {
+            get => _rndTwo;
+            set => SetProperty(ref _rndTwo, value);
+        }
+
+        public int RndThree
+        {
+            get => _rndThree;
+            set => SetProperty(ref _rndThree, value);
+        }
+
+        public ObservableCollection<int> ListOne
+        {
+            get => _listOne;
+            set => SetProperty(ref _listOne, value);
+        }
+
+        public ObservableCollection<int> ListTwo
+        {
+            get => _listTwo;
+            set => SetProperty(ref _listTwo, value);
+        }
+
+        public ObservableCollection<int> ListThree
+        {
+            get => _listThree;
+            set => SetProperty(ref _listThree, value);
+        }
+
     }
 }
--- HelloMvvm2/Domain/RandomView/RandomViewModel.cs
+++ HelloMvvm2/Domain/RandomView/RandomViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 using HelloMvvm2.Base;
 using HelloMvvm2.Domain.Models;
 
@@ -9,19 +12,31 @@ namespace HelloMvvm2.Domain.RandomView
     public class RandomViewModel : ViewModelBase, IRandomViewModel
     {
         private int _rndNumber;
+        
+        private DispatcherTimer _timer;
 
         private RandomModel _myModel;
 
         private ObservableCollection<RandomModel> _myModels;
-
+    
+        [DesignOnly(true)]
         public RandomViewModel()
         {
+            MyModel = new RandomModel()
+            { 
+                RndNumber = new Random(),
+                RndOne = new int(),
+                RndTwo = new int(),
+                RndThree = new int(),
+                ListOne = new ObservableCollection<int>(),
+                ListTwo = new ObservableCollection<int>(),
+                ListThree = new ObservableCollection<int>()
+            };
 
             RandomizeNumber = new DelegateCommand(RandomizeNumberExecuted, RandomizeNumberCanExecute);
         }
 
         public DelegateCommand RandomizeNumber { get; set; }
-
 
         public int RndNumber
         {
@@ -41,19 +56,49 @@ namespace HelloMvvm2.Domain.RandomView
             set => SetProperty(ref _myModels, value);
         }
 
+        private async Task LoadRandomsAndStartTimer()
+        {
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(5) };
+            _timer.Tick -= LoadRandomsTimerExecute;
+            _timer.Tick += LoadRandomsTimerExecute;
+            _timer.Start();
 
+        }
 
+        private async void LoadRandomsTimerExecute(object sender, EventArgs e)
+        {
+            try
+            {
+                _timer.Stop();
+                await LoadRandomNumbers();
+            }
+            catch (Exception)
+            {
+                // ?
+            }
+            finally { _timer.Start(); }
+        }
 
+        private async Task LoadRandomNumbers()
+        {
+            MyModel.RndOne = MyModel.RndNumber.Next(0, 100);
+            MyModel.RndTwo = MyModel.RndNumber.Next(0, 100);
+            MyModel.RndThree = MyModel.RndNumber.Next(0, 100);
+
+            AddToList();
+        }
+
+        public void AddToList()
+        {
+            MyModel.ListOne.Add(MyModel.RndOne);
+            MyModel.ListTwo.Add(MyModel.RndTwo);
+            MyModel.ListThree.Add(MyModel.RndThree);
+        }
 
         private void RandomizeNumberExecuted(object model)
         {
             if (RandomizeNumberCanExecute(true))
             {
-                MyModel = new RandomModel()
-                {
-                    RndNumber = new Random()
-                };
-
                 RndNumber = MyModel.RndNumber.Next(0, 100);
             }
         }
@@ -63,9 +108,9 @@ namespace HelloMvvm2.Domain.RandomView
             return true;
         }
 
-        public Task Loaded()
+        public async Task Loaded()
         {
-            throw new NotImplementedException();
+            await LoadRandomsAndStartTimer();
         }
 
         public Task Unloaded()
